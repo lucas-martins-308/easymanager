8bf347c340fc4d8a60213884f816df0478adf31c
Lucas Martins <lucasmartins308@gmail.com>
2024-09-23 20:13:21 -0300

    Componente basico navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import TopBar from './components/TopBar/TopBar'
 
 export default function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <TopBar/>
   )
 }
