010cc70d617eedb1097d38983018687d0455824f
LUCAS DE ANDRADE MARTINS <lucas_de-martins@estudante.sesisenai.org.br>
2024-10-01 21:47:30 -0300

    Criando componentes de tabela e estoque

--- src/components/Stock/Stock.jsx
+++ src/components/Stock/Stock.jsx
@@ -1,5 +1,11 @@
+import Table from './Table/Table';
 import './index.css';
 
 export default function Stock() {
+    return(
+        <div className='stock'>        
+            <Table/>
+        </div>
 
+    )
 }
