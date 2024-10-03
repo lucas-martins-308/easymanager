e2d1cf0db8f6532675b9069fad86914e27189bbf
LUCAS DE ANDRADE MARTINS <lucas_de-martins@estudante.sesisenai.org.br>
2024-10-02 21:45:08 -0300

    Replicando componentes

---
+++ src/components/ViewingNotes/ViewingCheckOut/ViewingNotes.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+export default function ViewingNotes() {
+
+    return(
+        <>
+            <div id='viewing-notes'>
+                <h1>Titulo</h1>
+            </div>
+        </>
+    )
+}
