d391c69af0907acfea2be46514bc5e2bc4aa3af6
Lucas Martins <lucasmartins308@gmail.com>
2024-11-25 15:38:27 -0300

    Corrigindo componentes e aplicando router

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,24 +1,13 @@
 import './index.css';
-import logo from '../../assets/Logo_Art_Hostel_Abaporu.png';
-import Navbar from '../TopBar/Navbar/Navbar.jsx'
-import DropdownButton from './DropdownButton/DropdownButton.jsx';
-import App from '../../App.jsx'
+import Navbar from '../Navbar/Navbar.jsx'
 
-export default function TopBar({ setCurrentPage, handleLogout }) {
+ function TopBar() {
 
   return (
     <div className="topbar">
-      <img src={logo} alt="Logo" className="topbar-logo" />
-      <Navbar setCurrentPage={setCurrentPage} />
-      <DropdownButton id="navbar" title="Usuário">
-        <button className="navbar-button" onClick={() => setCurrentPage('map')} type='button'>Ação 1</button>
-        <button className="navbar-button" onClick={() => setCurrentPage('map')} type='button'>Ação 1</button>
-        <button className="navbar-button" onClick={() => setCurrentPage('map')} type='button'>Ação 1</button>
-        <button className="navbar-button" onClick={() => setCurrentPage('map')} type='button'>Ação 1</button>
-        <button className="navbar-button" onClick={() => handleLogout()} type='button'>Sair</button>
-      </DropdownButton>
-      
-      <DropdownButton/>
+      <Navbar/>
     </div>
   );
 }
+
+export default TopBar;
